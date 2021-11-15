9db4f3ce7489b08eaaf95184701e3e81aca28869
p22jdiao <p22jdiao@fairfieldprep.org>
2021-11-15 08:38:52 -0500

    finished

---
+++ hw6-2.py
@@ -0,0 +1,31 @@
+# Author: JD 11/15/2021
+
+# Question 1
+li = [1,2,3,4,5,6,7]
+
+# Question 2
+print(len(li))
+
+# Question 3
+print(li[2])
+
+# Question 4
+print(not(5 in li))
+
+# Question 5
+print(not(10 in li))
+
+# Question 6
+li2 = ['Jason', 'Larry', 'Mateo', 'Michael']
+
+# Question 7
+print(li2[0])
+
+# Question 8
+print(li2 * 3)
+
+# Question 9
+li.extend(li2)
+
+# Question 10
+print(len(li))
